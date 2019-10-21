bb45bf4d66bdcc953b5f333846c864f383aa32da
Miguel Marques <migsmarques@gmail.com>
2019-10-21 03:41:56 +0100

    Fix Header

--- mclock.h
+++ mclock.h
@@ -1,3 +1,5 @@
+#ifndef MCLOCK_H_
+#define MCLOCK_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,3 +22,5 @@ int locate_dram(int page_id, char *rw, pte **table, int size, int *dram_writes);
 int locate_nvram(int page_id, char *rw, pte **nvram, int nvram_size, pte **dram, int dram_size, int *dram_writes, int *nvram_writes);
 int place(int page_id, char *rw, pte **table, int size, int *dram_writes);
 int mclock(int page_id, char *rw, pte **dram, int dram_size, pte **nvram, int nvram_size, int *dram_writes);
+
+#endif
